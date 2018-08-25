9dda2eb551ac4f268a75695463293adab667fe8b
DanielCamelCase <42586858+DanielCamelCase@users.noreply.github.com>
2018-08-25 08:36:47 +0200

    Update and rename Prueba0.java to holamundo.java

---
+++ holamundo.java
@@ -0,0 +1,12 @@
+package pruebasIniciales; 
+
+// esto es una clase de java en todo proyecto tiene que haber al menos una clase obligatoriamente 
+public class Prueba0 { 
+
+	public static void main (String args[]) { // se indica que se pasara una cadena de texto (string)
+		
+		System.out.print("hola mundo"); // de esta menera imprimimos un mensaje por consola 
+		
+	}
+	
+}	
