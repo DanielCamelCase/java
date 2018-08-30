6c8dca37d296204f94fa804f4f345559f1689c62
DanielCamelCase <42586858+DanielCamelCase@users.noreply.github.com>
2018-08-30 23:19:33 +0200

    Add files via upload

---
+++ Calculo_RedondearAXDecimales.java
@@ -0,0 +1,17 @@
+package pruebasIniciales;
+
+public class Calculo_RedondearAXDecimales {
+
+	public static void main(String[] args) {
+		//
+		double x=10000.0;
+		 
+		// Para hacer redondeos de un decimal se usa printf
+		// y %1.2 donde .2 son los decimales que quieres mostrar 
+		
+		System.out.printf("%1.2f", x/3);//el resultado es 3333.33
+		
+		//video 15 minuto 18:30 pildoras
+	}
+
+}
