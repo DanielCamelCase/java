366bbaca5268014b50978a2089c1f975695f4ebe
DanielCamelCase <42586858+DanielCamelCase@users.noreply.github.com>
2018-09-04 20:01:37 +0200

    Update Bucles_while_Acertar_Numeros.java

--- Bucles_while_Acertar_Numeros.java
+++ Bucles_while_Acertar_Numeros.java
@@ -8,6 +8,8 @@ public class Bucles_while_Acertar_Numeros {
 
 	public static void main(String[] args) {
 		
+		// random devuelve un aleatorio entre 0 y 1 de caracter double por lo que lo que devuelve lo multiplicamos por 100 para que tenga
+		// una parte entera y con el =(int) hacemos una refundicion de double en un entero 
 		int aleatorio =(int)(Math.random()*100);
 		
 		Scanner entrada =new Scanner(System.in);
@@ -35,7 +37,8 @@ public class Bucles_while_Acertar_Numeros {
 				System.out.println("El numero que buscas es mas Alto");
 			}
 		}
-		
+		// en el archivo "Juego_adivinar_numero_ejercicios" introduciremos un limite para que 
+		// cuando el jugador lo pase pierda 
 		System.out.println("Has acertado, Felicidades lo has conseguido en " + intentos);
 		JOptionPane.showMessageDialog(null, "Lo conseguiste en "+ intentos+ "intentos");
 	}
